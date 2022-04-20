d23f673727aac296d1778228ba606872f0b2ac19
AG <100623549+aleksigoulet@users.noreply.github.com>
2022-04-20 18:59:18 -0400

    add winds and vis interface

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
-// let metar = 'SPECI CYTZ 190039Z AUTO 07016G22KT 1 1/4SM -RA OVC004 02/01 A2990 RMK SLP128='
-
-
-
-// metar = metar.split(' ');
-
-// console.log(metar[5]);
-
-// let button = document.getElementById("submitButton");
-
-// // document.getElementById('result').innerHTML = "TEST"
-// let input = '';
-
-// button.addEventListener('click', (ev)=>{
-//     ev.preventDefault();
-//     // console.log('test');
-//     // console.log(document.getElementById('metarInput').value)
-//     input = document.getElementById('metarInput').value;
-//     input = input.split(' ');
-//     document.getElementById('result').innerHTML = input[5];
-// })
-
-
-
-
-
-
-
-
 ////////////////
 ///Production///
 ////////////////
 
-
 //Import necessary functions
-// const MetarDecode = require('./modules/decode-metar.js');
-// const csvToArray = require('./modules/csvToArray.js');
 
 import { MetarDecode } from "./modules/decode-metar.js";
 import { csvToArray } from "./modules/csvToArray.js";
@@ -61,8 +29,12 @@ csvSubmit.addEventListener('submit', ev => {
         }
         console.log(data);
 
-        const conditions = decoder.getCond(data);
-        document.getElementById('result').innerHTML = `${conditions.total} days analyzed, ${conditions.ifr} days were IFR and ${conditions.vfr} days were VFR.`;
+        const conditions = decoder.getCond(data, 20);
+
+        //display results to user
+        document.getElementById('resultVis').innerHTML = `${conditions.total} days analyzed, ${conditions.ifr} days had an average of IFR visibility and ${conditions.vfr} days had an average of VFR visibility.`;
+
+        document.getElementById('resultWind').innerHTML = `${conditions.total} days analyzed, ${conditions.windLess} days had average winds less than ${conditions.windThreshold} KTS and ${conditions.windMore} days had average winds greater than or equal to ${conditions.windThreshold} KTS.`;
 
 
     };
@@ -70,60 +42,11 @@ csvSubmit.addEventListener('submit', ev => {
     reader.readAsText(input);
 })
 
-
-
-
-
-
-
 /////////////
 ///Testing///
 /////////////
 
-
-let result = {
-    input: '',
-    getVis: function(metarStr){ //function to get visibility as number
-        //extract visibility string using regex//
-        this.input = metarStr;
-
-        const regex = /(?<!\d\d\dV\d\d\d )\b\s?\d?\s\d*\/?\d*SM/;
-        let vis = this.input.match(regex)[0].trim(); //matches regex, trims string and stores in vis variable
-
-        //remove SM from vis
-        vis = vis.slice(0, -2);
-
-        //Create array with two parts of number
-        vis = vis.split(' ');
-
-        if(vis.length > 1){
-            //convert to decimal number string and remove 0
-            vis[vis.length - 1] = vis[vis.length - 1].split('/').reduce((a, c) => a/c);
-            vis[vis.length - 1] = vis[vis.length - 1].toString().slice(1);
-
-            vis = vis[0].concat(vis[1]);
-        } else {
-            vis = vis[0].split('/').reduce((a, c) => a/c);
-        }
-
-        vis = parseFloat(vis);
-
-        return vis
-    }
-}
-
-
-// let metar = 'CYTZ 100700Z AUTO 29009KT 240V300 9SM OVC040 02/M01 A2979 RMK WX MISG VIS MISG PCPN MISG SLP094'
+//Import necessary functions
 
 // const MetarDecode = require('./modules/decode-metar.js');
-
-
-// // module.exports = result;
-
-// const bugFix = new MetarDecode();
-
-// console.log(bugFix.getVis(metar));
-
-// console.log(result.getVis(metar));
-
-
+// const csvToArray = require('./modules/csvToArray.js');
